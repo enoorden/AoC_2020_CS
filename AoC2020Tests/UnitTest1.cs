9c9c884f148d5b1b911ea4c4e385defaa9a0027d
Erik <erik.vannoorden@ogd.nl>
2020-12-09 13:59:12 +0100

    day9

---
+++ AoC2020Tests/UnitTest1.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+using Day09;
+
+namespace AoC2020Tests
+{
+    public class UnitTest1
+    {
+        long[] Range = new long[] { 1, 2, 3, 4, 5 };
+
+        [Fact]
+        public void RangeContainsSum1()
+        {
+            var result = day09.RangeContainsSum(Range, 9);
+            Assert.True(result, "should be true");
+        }
+        [Fact]
+        public void RangeContainsSum2()
+        {
+            var result = day09.RangeContainsSum(Range, 10);
+            Assert.False(result, "should be false");
+        }
+        [Fact]
+        public void RangeContainsSum3()
+        {
+            var result = day09.RangeContainsSum(Range, 6);
+            Assert.True(result, "should be true");
+        }
+        [Fact]
+        public void RangeContainsSum4()
+        {
+            var result = day09.RangeContainsSum(Range, 2);
+            Assert.False(result, "should be false");
+        }
+    }
+}
---
+++ Day09/day09.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day09
+{
+    public class day09
+    {
+        static long[] input = File.ReadAllLines(@"..\..\..\input09.txt").Select(t => Convert.ToInt64(t)).ToArray();
+        private static int back = 25;
+
+        static void Main(string[] args)
+        {
+            //part1
+            long n = 0;
+            for (var i = back; i < input.Length; i++)
+            {
+                n = input[i];
+                var r = input[(i - back)..i];
+                
+                if (RangeContainsSum(r, n)) continue;
+                Console.WriteLine($"Part1: {n}");
+                break;
+            }
+
+            //part2
+            for (var i = 0; i < input.Length; i++)
+            {
+                var total = input[i];
+                var j = 1;
+                while (total < n)
+                {
+                    total += input[i + j];
+                    if (total == n)
+                    {
+                        Console.WriteLine($"Part2: {input[i..(i + j + 1)].Min() + input[i..(i + j + 1)].Max()}");
+                        return;
+                    }
+                    j++;
+                }
+            }
+        }
+
+        public static bool RangeContainsSum(long[] range, long sum)
+        {
+            return range.Any(x => range.Contains(sum - x) && sum - x != x);
+        }
+    }
+}
